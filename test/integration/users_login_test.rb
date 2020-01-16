73fdf63a20cddcfc98785107721af5f9e6905b61
tokunagakouya <kouyatokunaga@gmail.com>
2020-01-16 15:23:43 +0900

    lesson10

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -29,7 +29,6 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   end
 
   test "login with remembering" do
-    debugger
     log_in_as(@user, remember_me: '1')
     assert_equal cookies['remember_token'], assigns(:user).remember_token
   end
