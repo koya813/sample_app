d7eafc33f57a8c260281b7226c2efb69bd943716
tokunagakouya <kouyatokunaga@gmail.com>
2020-01-19 01:38:25 +0900

    lesson10

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  
+
   def setup
-    @user = users(:michael)
+    @user       = users(:michael)
     @other_user = users(:archer)
   end
 
