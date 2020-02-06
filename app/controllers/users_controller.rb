409702ae57aebb05358f086319f72e6411bb4b00
tokunagakouya <kouyatokunaga@gmail.com>
2020-02-06 15:08:39 +0900

    lesson13

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,27 +49,21 @@ class UsersController < ApplicationController
 
   private
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
-    end
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
 
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
+  # beforeフィルター
 
-    def correct_user
-      @user = User.find(params[:id])
-      redirect_to(root_url) unless current_user?(@user)
-    end
+  # 正しいユーザーかどうかを確認
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
 
-    # 管理者かどうか確認
-    def admin_user
-      redirect_to(root_url) unless current_user.admin?
-    end
+  # 管理者かどうかを確認
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
 end
-
