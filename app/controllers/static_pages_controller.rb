3462f73dd426a48f708ec67d72fa01f0df5f95ac
tokunagakouya <kouyatokunaga@gmail.com>
2020-02-06 16:07:32 +0900

    lesson13

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,15 +1,18 @@
 class StaticPagesController < ApplicationController
+
   def home
-    @micropost = current_user.microposts.build if logged_in?
+    if logged_in?
+      @micropost  = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
   end
 
   def help
   end
 
   def about
-
   end
+
   def contact
-  
   end
 end
