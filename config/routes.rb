2e85ef6bb9167fab87d2020fe960eee62db15462
tokunagakouya <kouyatokunaga@gmail.com>
2020-02-12 19:31:15 +0900

    lesson14

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
       get :following, :followers
     end
   end
+  resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
